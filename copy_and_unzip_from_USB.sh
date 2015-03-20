f21aad4744f71708033126b63241c528187271be
Hoel <hoel.iris@gmail.com>
2015-03-20 12:59:58 +0100

    Raspberry README update

---
+++ copy_and_unzip_from_USB.sh
@@ -0,0 +1,6 @@
+cp /media/removable/USB\ Drive/Makespace.zip .
+7za x Makespace.zip -oMakespace
+rm Makespace.zip
+
+
+
---
+++ zip_and_copy_to_USB.sh
@@ -0,0 +1,6 @@
+rm Makespace.zip
+7za a -P -tzip Makespace.zip Makespace
+
+cp Makespace.zip /media/removable/USB\ Drive/.
+
+
